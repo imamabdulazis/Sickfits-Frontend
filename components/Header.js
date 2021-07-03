f478ed8ef89f4ffbc09e76bf86d9edbacc19b6c0
imam <imamabdulazis028@gmail.com>
2021-07-04 00:54:19 +0700

    init

--- components/Header.js
+++ components/Header.js
@@ -21,6 +21,16 @@ const Logo = styled.h1`
 const HeaderStyles = styled.header`
   .bar{
     border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .sub-bar{
+    display: grid;
+    grid-template-columns:1fr auto;
+    border-bottom: 1px solid var(--black, black);
   }
 `;
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,16 @@
 import Page from "../components/Page";
+import Router from 'next/router';
+import Nprogress from 'nprogress';
+
+///TODO : default style progress
+// import 'nprogress/nprogress.css';
+///TODO : custom style progress
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => Nprogress.start());
+Router.events.on('routeChangeComplete', () => Nprogress.done());
+Router.events.on('routeChangeError', () => Nprogress.done());
+
 
 export default function MyApp({ Component, pageProps }) {
   return (
